ec07e039a4d43d729a6bbddfbf514adfdcf474ac
İbrahim SARI <pr.ibrahimsari@gmail.com>
2021-12-27 15:29:09 +0300

    subscribe to new links

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from "./Link";
 import {useQuery, gql} from "@apollo/client";
+import {LINKS_PER_PAGE} from "../constants";
+import {useLocation} from "react-router-dom";
 
 export const FEED_QUERY = gql`
     {
@@ -26,9 +28,78 @@ export const FEED_QUERY = gql`
   }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+`;
+
+const getQueryVariables = (isNewPage, page) => {
+  const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+  const take = isNewPage ? LINKS_PER_PAGE : 100;
+  const orderBy = { createdAt: 'desc' };
+  return { take, skip, orderBy };
+};
+
 const LinkList = () => {
 
-  const {data} = useQuery(FEED_QUERY);
+  //page, history, index, pagination
+  const location = useLocation();
+  const isNewPage = location.pathname.includes(
+    'new'
+  );
+  const pageIndexParams = location.pathname.split(
+    '/'
+  );
+  const page = parseInt(
+    pageIndexParams[pageIndexParams.length - 1]
+  );
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE : 0;
+
+
+  const {
+    data,
+    loading,
+    error,
+    subscribeToMore
+  } = useQuery(FEED_QUERY, {
+    variables: getQueryVariables(isNewPage, page)
+  });
+
+  subscribeToMore({
+    document: NEW_LINKS_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+      const newLink = subscriptionData.data.newLink;
+      const exists = prev.feed.links.find(
+        ({ id }) => id === newLink.id
+      );
+      if (exists) return prev;
+
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename
+        }
+      });
+    }
+  });
 
   return (
       <div>
--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import {
     ApolloProvider,
     ApolloClient,
     createHttpLink,
-    InMemoryCache
+    InMemoryCache,
+    split
 } from '@apollo/client';
 import {setContext} from "@apollo/client/link/context";
 import {BrowserRouter} from "react-router-dom";
 import {AUTH_TOKEN} from "./constants";
-
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
@@ -27,9 +29,31 @@ const authLink = setContext((_, {headers})=>{
     }
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+});
+
+const link = split(
+  ({query})=>{
+    const {kind, operation} = getMainDefinition(query);
+    return (
+      kind === "OperationDefinition" &&
+      operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
+  link,
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
